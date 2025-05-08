6b899366e66a249508791975c8cc24c81310afe0
majorek31 <m4y0r.offical@gmail.com>
2025-05-08 15:50:09 +0200

    feat: make specific field required

--- src/WebAPI/Entities/RefreshToken.cs
+++ src/WebAPI/Entities/RefreshToken.cs
@@ -4,9 +4,9 @@ namespace WebAPI.Entities;
 
 public class RefreshToken : BaseEntity
 {
-    public string Token { get; set; }
+    public required string Token { get; set; }
     
-    public string UserAgent { get; set; }
+    public required string UserAgent { get; set; }
     public DateTime ExpiresAt { get; set; }
     public bool IsRevoked { get; set; }
     public int UserId { get; set; }
